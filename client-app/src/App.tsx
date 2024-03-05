8313f9957d0655b2c043f50785fb394a31b1aa77
Racianu Marian Gabriel <94560373+RacianuGabriel@users.noreply.github.com>
2024-03-05 19:59:28 +0200

    small modification

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import icon from './icon.svg';
 
 function App() {
   const [workouts, setWorkouts] = useState<any[]>([]);
@@ -19,15 +21,45 @@ function App() {
 
   return (
     <div>
-      <Container>
+      <Navbar expand="lg" className='bg-body-tertiary'>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={icon}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="FitWeb"
+            />
+            FitWeb
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <ListGroup>
           {workouts.map(workout => (
-              <Row key={workout.id}>
-                <Col>
-                  {workout.title}
-                </Col>
-              </Row>
+              <ListGroup.Item key={workout.id}>
+                {workout.title}
+              </ListGroup.Item>
               ))} 
-      </Container>
+      </ListGroup>
     </div>
   );
 }
